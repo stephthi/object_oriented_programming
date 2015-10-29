d4ce51430b3c0f98a5def9affa7175ad988bc946
stephthi <nstephanie2@gmail.com>
2015-10-29 15:58:45 -0400

    Both rovers input and output

--- rover.rb
+++ rover.rb
@@ -1,15 +1,14 @@
 class Rover
 	attr_accessor	:x, :y, :direction, :name
 
-	def initialize(x, y, direction, name)
+	def initialize(x, y, direction)
 		@x = x
 		@y = y
 		@direction = direction
-		@name = name
 	end
 
 	def location
-		puts "The Rover #{name} is located at [#{x}, #{y}] facing #{direction}."
+ 		"[#{x}, #{y}] facing #{direction}."
 	end
 
 
@@ -24,21 +23,25 @@ class Rover
 	end
 
 
-	def move(input)
-		if input == "N"
-			puts "[#{@x}, #{@y + 1}]"
-		elsif input == "S"
-			puts "[#x, #{@y - 1}]"
-		elsif input == "W"
-			puts "[#{@x - 1}, #{y}]"
-		elsif input == "E"
-			puts "[#{@x + 1}, #{y}]"
+	def move
+		if @direction == "N"
+			@y += 1
+			puts "[#{@x}, #{@y}]"
+		elsif @direction == "S"
+			@y -= 1
+			puts "[#{x}, #{@y}]"
+		elsif @direction == "W"
+			@x -= 1
+			puts "[#{@x}, #{y}]"
+		elsif @direction == "E"
+			@x += 1
+			puts "[#{@x}, #{y}]"
 		else
 				puts "This action is invalid. Please enter a direction."
 		end
 	end
 
-		def turn(input)
+	def turn(input)
 		if input == "L"
 			if @direction == "W"
 				@direction = "S"
@@ -75,6 +78,41 @@ class Plateau
 		puts "This plateau is #{@x_width} by #{@y_height}."
 	end
 end
+
+plateau1 = Plateau.new(5, 5)
+plateau1.size_info
+
+puts "1. What is the x coordinate, y coordinate and direction?"
+answer1 = gets.chomp
+
+# answer1.split("").each do |var|
+# 	rover1.read(var)
+# end
+
+rover1 = Rover.new(answer1[0].to_i, answer1[2].to_i, answer1[4])
+
+puts "What the sequence of turns and moves you would like to do?"
+travel1 = gets.chomp
+
+travel1.split("").each do |var|
+	rover1.read(var)
+end
+
+puts "2. What is the x coordinate, y coordinate and direction?"
+answer2 = gets.chomp
+
+rover2 = Rover.new(answer2[0].to_i, answer2[2].to_i, answer2[4])
+
+puts "What the sequence of turns and moves you would like to do?"
+travel2 = gets.chomp
+
+travel2.split("").each do |var|
+	rover2.read(var)
+end
+
+puts rover1.location
+puts rover2.location
+
 
 
 
