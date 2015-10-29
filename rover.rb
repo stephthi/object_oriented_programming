f1f7bc1a6b7f79d84c3be2c6b37d768d774a170b
stephthi <nstephanie2@gmail.com>
2015-10-29 14:27:40 -0400

    Update move function

--- rover.rb
+++ rover.rb
@@ -1,4 +1,5 @@
 class Rover
+	attr_accessor	:x, :y, :direction, :name
 
 	def initialize(x, y, direction, name)
 		@x = x
@@ -7,14 +8,45 @@ class Rover
 		@name = name
 	end
 
+	def location
+		puts "The Rover #{name} is located at [#{x}, #{y}] facing #{direction}."
+	end
+
+
 	def read_instruction(input)
-		if input == "L"
-			move(input)
-		elsif
-		 	 input == "M"
+		if input == "L" || input == "R"
+			turn(input)
+		elsif input == "M"
+			move
 		else
 			puts "This is an invalid action."
 		end
 	end
 
+	def move(input)
+		if input == "N"
+			puts "[#{@x}, #{@y + 1}]"
+		elsif input == "S"
+			puts "[#x, #{@y - 1}]"
+		elsif input == "W"
+			puts "[#{@x - 1}, #{y}]"
+		elsif input == "E"
+			puts "[#{@x + 1}, #{y}]"
+		else
+				puts "This action is invalid. Please enter a direction."
+		end
+	end
+
+# 	def turn(input)
+# 		if input == "L"
+# 			@y - 1
+# 		elsif input == "R"
+# 			@position + 1
+# 		else
+# 			puts "Your rover cannot turn."
+# 		end
+# end
 end
+
+
+
